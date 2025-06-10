6bddf21baadf252bc63d7279922e9a4825165971
HM-Agency <hugo.marie2108@gmail.com>
2025-06-10 12:57:03 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function openProject(projectId) {
+  alert(`Ouverture du projet : ${projectId}`);
+  // Remplacez alert par la navigation réelle.
+}
+
+function openArticle(articleId) {
+  alert(`Ouverture de l'article : ${articleId}`);
+  // Remplacez alert par la navigation réelle.
+}
